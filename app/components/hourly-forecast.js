6de8af5a1f50a6dfa57352838a757374ff6bfff5
Austin <austineady@github.com>
2015-06-26 12:32:46 -0400

    Working on Hourly

--- app/components/hourly-forecast.js
+++ app/components/hourly-forecast.js
@@ -2,5 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['hourly-weather-container']
+  classNames: ['hourly-weather-container'],
+  upcomingForecasts: function() {
+    return this.get('model.data').slice(0, 12);
+  }.property('model.data')
 });
