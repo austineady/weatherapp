036352405d4639492b0608e0b80447bf27949e79
Austin <austineady@github.com>
2015-06-26 16:04:35 -0400

    Working on styling and hourly

--- app/components/current-weather.js
+++ app/components/current-weather.js
@@ -2,5 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName: 'section',
-  classNames: ['current-weather-container']
+  classNames: ['current-weather-container'],
+  isShowing: true
 });
--- app/components/hourly-forecast.js
+++ app/components/hourly-forecast.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   tagName: 'section',
   classNames: ['hourly-weather-container'],
+  isShowing: false,
   upcomingForecasts: function() {
     return this.get('model.data').slice(0, 12);
   }.property('model.data')
