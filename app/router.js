581ee1cb688cfede581fa4b89f6fca6fcf1ac85a
Austin <austineady@github.com>
2015-06-26 11:14:57 -0400

    Working on routing

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('current', { path: '/'});
+  this.route('hourly');
+  this.route('weekly');
 });
 
 export default Router;
--- app/routes/application.js
+++ app/routes/application.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    // return Ember.RSVP.hash({
-    //   items: Ember.$.ajax("https://api.forecast.io/forecast/249830a432373c0331ab2cf8b33a624e/34.8520289,-82.3996694", {dataType: 'jsonp'}),
-    //   location: new Ember.RSVP.Promise(function(resolve, reject) {
-    //     navigator.geolocation.getCurrentPosition(resolve, reject);
-    //   })
-    // });
+    return Ember.RSVP.hash({
+      items: Ember.$.ajax("https://api.forecast.io/forecast/249830a432373c0331ab2cf8b33a624e/34.8520289,-82.3996694", {dataType: 'jsonp'}),
+      location: new Ember.RSVP.Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      })
+    });
   }
 });
 
